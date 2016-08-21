24e3c763b274fe5e9d5ad08652424983f99517b8
340StarObserver <Seven_lv0330@163.com>
2016-08-21 16:42:25 +0800

    oss operation

---
+++ src/common/configure.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+# Author 		: 	Lv Yang
+# Created 		: 	21 August 2016
+# Modified 		: 	21 August 2016
+# Version 		: 	1.0
+
+"""
+This script used to read configuration from a file
+"""
+
+import ConfigParser
+import sys
+
+
+def read(filename):
+    """
+    this function used to read configuration from a file
+
+    the parameter is the path of file
+    it returns a dictionary of pairs of <key,value>
+    """
+    res = {'oss':{}}
+    config = ConfigParser.ConfigParser()
+    try:
+        config.read(filename)
+        res['oss']['access_id'] = config.get('oss','access_id')
+        res['oss']['access_key'] = config.get('oss','access_key')
+        res['oss']['end_point'] = config.get('oss','end_point')
+        res['oss']['bucket_name'] = config.get('oss','bucket_name')
+        res['oss']['shared_dir']=config.get('oss','shared_dir')
+        res['oss']['static_dir']=config.get('oss','static_dir')
+    except Exception,e:
+        print "fail to read configuration from %s"%(filename)
+        print str(e)
+        sys.exit(1)
+    return res
+
+
+if __name__ == '__main__':
+    conf = read("../conf/server.conf")
+    print conf
